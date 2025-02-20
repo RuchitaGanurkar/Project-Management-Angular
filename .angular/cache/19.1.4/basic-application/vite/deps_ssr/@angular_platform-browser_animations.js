16d13cd25e4177898877a141c1038ecd5bf92622
RuchitaGanurkar <rmganukar1323@gmail.com>
2025-02-20 12:25:14 +0530

    To be deployed

---
+++ .angular/cache/19.1.4/basic-application/vite/deps_ssr/@angular_platform-browser_animations.js
@@ -0,0 +1,25 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-Y24YMQS4.js";
+import "./chunk-QAAFUZT6.js";
+import "./chunk-N4RD55UT.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-5D2JJTZD.js";
+import "./chunk-WBQSER3X.js";
+import "./chunk-TDK5NIWS.js";
+import "./chunk-RK6XMIZN.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
